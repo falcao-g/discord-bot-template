40fe577a2b359f5e188e21291136d88800180812
Falcão <gabriel.p.falcao50@gmail.com>
2023-12-12 16:20:21 -0400

    refactor: commands and events handlers

---
+++ src/handlers/eventsHandler.js
@@ -0,0 +1,27 @@
+class EventsHandler {
+	// this is the event handler, it will load all the events from the events folder
+	async loadEvents(instance, client) {
+		const { loadFiles } = require("../utils/fileLoader")
+
+		await client.events.clear()
+
+		const Files = await loadFiles("events")
+
+		Files.forEach((file) => {
+			const event = require(file)
+
+			const execute = (...args) => event.execute(...args, instance, client)
+			client.events.set(event.name, execute)
+
+			if (event.once) {
+				client.once(event.name, execute)
+			} else {
+				client.on(event.name, execute)
+			}
+
+			console.log(`Event: ${event.name} ✅`)
+		})
+	}
+}
+
+module.exports = { EventsHandler }
